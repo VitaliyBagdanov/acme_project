9b9025adaebff5755a7b9925f1c38e136a3627bf
Vitaliy Bagdanov <vitaliy.bagdanov@yandex.ru>
2024-09-17 19:52:44 +0300

    up_edit_delete

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .forms import BirthdayForm
 from .utils import calculate_birthday_countdown
 from .models import Birthday
@@ -18,6 +18,23 @@ def birthday(request, pk=None):
         )
         context.update({'birthday_countdown': birthday_countdown})
 
+    return render(request, 'birthday/birthday.html', context)
+
+
+def delete_birthday(request, pk):
+    # Получаем объект модели или выбрасываем 404 ошибку.
+    instance = get_object_or_404(Birthday, pk=pk)
+    # В форму передаём только объект модели;
+    # передавать в форму параметры запроса не нужно.
+    form = BirthdayForm(instance=instance)
+    context = {'form': form}
+    # Если был получен POST-запрос...
+    if request.method == 'POST':
+        # ...удаляем объект:
+        instance.delete()
+        # ...и переадресовываем пользователя на страницу со списком записей.
+        return redirect('birthday:list')
+    # Если был получен GET-запрос — отображаем форму.
     return render(request, 'birthday/birthday.html', context)
 
 
